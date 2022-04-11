1bbf91ff0501999d67c96311d8af18feb03b2a94
coder_s <53449550+simonebatra@users.noreply.github.com>
2022-04-10 18:00:57 -0700

    Add files via upload

---
+++ Music Run/script.js
@@ -0,0 +1,35 @@
+var canvas;
+var backgroundImg;
+var player1, player1Img, player2, player2Img;
+var notesLevel = 4;
+
+function preload(){
+    backgroundImg = loadImage("Music Run Track.png");
+    player1Img = loadImage("man.png");
+    player2Img = loadImage("daughter.png");
+}
+
+function setup(){
+    canvas = createCanvas(windowWidth, windowHeight);
+
+    player1 = createSprite(525, 695);
+    player1.addImage(player1Img);
+    player1.scale = 0.2;
+
+    player2 = createSprite(925, 720);
+    player2.addImage(player2Img);
+    player2.scale = 0.2;
+}
+
+function draw(){
+    background(backgroundImg);
+
+    if (notesLevel>0){
+        rect(20,20, 200, 30);
+        fill("red");
+        rect(20,20, 200, 30);
+    }
+
+
+    drawSprites();
+}
